1e07d1d5ebb56714fdddcd3b9159edf73c6f66f3
Hryhorii-V <hryhorii.voloshyn@nure.ua>
2021-10-25 15:11:33 +0300

    Add project files.

---
+++ Rolling Tavern/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace Rolling_Tavern.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
