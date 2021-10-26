ac61dc377cb79afc4334bdb8e7ea86c1c83b0bc1
Yevhenii <tszheka@gmail.com>
2021-10-26 16:02:27 +0300

    Commit1

--- Rolling Tavern/Controllers/HomeController.cs
+++ Rolling Tavern/Controllers/HomeController.cs
@@ -33,5 +33,6 @@ namespace Rolling_Tavern.Controllers
         {
             return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
         }
+
     }
 }
