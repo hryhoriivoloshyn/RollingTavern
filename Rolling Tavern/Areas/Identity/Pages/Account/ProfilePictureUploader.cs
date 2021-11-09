c5e482fe0377d77e5af8762f679be05bf04b107e
Hryhorii-V <hryhorii.voloshyn@nure.ua>
2021-11-09 15:36:11 +0200

    Закончена загрузка фото при регистрации

---
+++ Rolling Tavern/Areas/Identity/Pages/Account/ProfilePictureUploader.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Rolling_Tavern.Areas.Identity.Pages.Account
+{
+    public class ProfilePictureUploader
+    {
+
+    }
+}
--- Rolling Tavern/Areas/Identity/Pages/Account/Register.cshtml.cs
+++ Rolling Tavern/Areas/Identity/Pages/Account/Register.cshtml.cs
@@ -1,15 +1,21 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
 using System.ComponentModel.DataAnnotations;
+using System.IO;
 using System.Linq;
+using System.Runtime.CompilerServices;
 using System.Text;
 using System.Text.Encodings.Web;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Binders;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.AspNetCore.WebUtilities;
 using Microsoft.Extensions.Logging;
@@ -20,6 +26,7 @@ namespace Rolling_Tavern.Areas.Identity.Pages.Account
     [AllowAnonymous]
     public class RegisterModel : PageModel
     {
+        private IWebHostEnvironment _appEnvironment;
         private readonly SignInManager<ApplicationUser> _signInManager;
         private readonly UserManager<ApplicationUser> _userManager;
         private readonly ILogger<RegisterModel> _logger;
@@ -29,12 +36,13 @@ namespace Rolling_Tavern.Areas.Identity.Pages.Account
             UserManager<ApplicationUser> userManager,
             SignInManager<ApplicationUser> signInManager,
             ILogger<RegisterModel> logger,
-            IEmailSender emailSender)
+            IEmailSender emailSender, IWebHostEnvironment appEnvironment)
         {
             _userManager = userManager;
             _signInManager = signInManager;
             _logger = logger;
             _emailSender = emailSender;
+            _appEnvironment = appEnvironment;
         }
 
         [BindProperty]
@@ -48,7 +56,7 @@ namespace Rolling_Tavern.Areas.Identity.Pages.Account
         {
             [Required]
             [EmailAddress]
-            [Display(Name = "Email")]
+            [Display(Name = "Електронна пошта")]
             public string Email { get; set; }
 
             
@@ -56,31 +64,35 @@ namespace Rolling_Tavern.Areas.Identity.Pages.Account
             [Required]
             [StringLength(32, ErrorMessage = "The {0} must be at max {1} characters long.")]
             [DataType(DataType.Text)]
-            [Display(Name = "First name")]
+            [Display(Name = "Ім'я*")]
             public string FirstName { get; set; }
 
 
             [Required]
             [StringLength(32, ErrorMessage = "The {0} must be at max {1} characters long.")]
             [DataType(DataType.Text)]
-            [Display(Name = "Last name")]
+            [Display(Name = "Призвище*")]
             public string LastName { get; set; }
 
             [Required]
             [DataType(DataType.Date)]
-            [Display(Name = "Date of birth")]
+            [Display(Name = "Дата народження*")]
             public DateTime DateOfBirth { get; set; }
 
             [Required]
             [StringLength(24, ErrorMessage = "The {0} must be at least {2} and at max {1} characters long.", MinimumLength = 6)]
             [DataType(DataType.Password)]
-            [Display(Name = "Password")]
+            [Display(Name = "Пароль*")]
             public string Password { get; set; }
 
             [DataType(DataType.Password)]
-            [Display(Name = "Confirm password")]
+            [Display(Name = "Повторіть пароль*")]
             [Compare("Password", ErrorMessage = "The password and confirmation password do not match.")]
             public string ConfirmPassword { get; set; }
+
+            [DataType(DataType.Upload)]
+            [Display(Name ="Завантажте фото профілю")]
+            public string ProfilePicture { get; set; }
         }
 
         public async Task OnGetAsync(string returnUrl = null)
@@ -89,20 +101,49 @@ namespace Rolling_Tavern.Areas.Identity.Pages.Account
             ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
         }
 
-        public async Task<IActionResult> OnPostAsync(string returnUrl = null)
+        private async Task<string> UploadPicture(IFormFile profilePicture)
+        {
+            const string defaultPicturePath = "/ProfilePictures/DefaultUser.png";
+            if (profilePicture == null)
+            {
+                return defaultPicturePath;
+            }
+
+            string pictureType = profilePicture.ContentType;
+            string pictureExtension = pictureType.Substring(pictureType.IndexOf("/") + 1);
+            string profilePicturePath = "/ProfilePictures/" + Input.Email + "." + pictureExtension;
+
+            using (var fileStream = new FileStream(_appEnvironment.WebRootPath + profilePicturePath, FileMode.Create))
+            {
+                await profilePicture.CopyToAsync(fileStream);
+            }
+
+            return profilePicturePath;
+        }
+
+        public async Task<IActionResult> OnPostAsync(IFormFile profilePicture, string returnUrl = null)
         {
             returnUrl ??= Url.Content("~/");
             ExternalLogins = (await _signInManager.GetExternalAuthenticationSchemesAsync()).ToList();
+            
+
             if (ModelState.IsValid)
             {
-                var user = new ApplicationUser { UserName = Input.Email, Email = Input.Email, FirstName = Input.FirstName, LastName = Input.LastName, DateOfBirth = Input.DateOfBirth};
+                
+
+                var picturePath = await UploadPicture(profilePicture);
+                var user = new ApplicationUser { UserName = Input.Email, Email = Input.Email, FirstName = Input.FirstName, LastName = Input.LastName, DateOfBirth = Input.DateOfBirth, ProfilePicture = picturePath };
+
                 var result = await _userManager.CreateAsync(user, Input.Password);
+                
                 if (result.Succeeded)
                 {
                     _logger.LogInformation("User created a new account with password.");
 
                     var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
+
                     code = WebEncoders.Base64UrlEncode(Encoding.UTF8.GetBytes(code));
+
                     var callbackUrl = Url.Page(
                         "/Account/ConfirmEmail",
                         pageHandler: null,
