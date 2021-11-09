d31acc5883cba02066ffbf9217def3907cde26c0
daywish <volodymyr.korobko@nure.ua>
2021-11-09 15:49:08 +0200

    нові файли

---
+++ Rolling Tavern/Controllers/MeetingController.cs
@@ -0,0 +1,166 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Rolling_Tavern.Data;
+using Rolling_Tavern.Models;
+
+namespace Rolling_Tavern.Controllers
+{
+    public class MeetingController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public MeetingController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        // GET: Meeting
+        public async Task<IActionResult> Index()
+        {
+            var applicationDbContext = _context.Meetings.Include(m => m.Creator).Include(m => m.Game);
+            return View(await applicationDbContext.ToListAsync());
+        }
+
+        // GET: Meeting/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var meeting = await _context.Meetings
+                .Include(m => m.Creator)
+                .Include(m => m.Game)
+                .FirstOrDefaultAsync(m => m.MeetingId == id);
+            if (meeting == null)
+            {
+                return NotFound();
+            }
+
+            return View(meeting);
+        }
+
+        // GET: Meeting/Create
+        public IActionResult Create()
+        {
+            ViewData["CreatorId"] = new SelectList(_context.Users, "Id", "Id");
+            ViewData["GameId"] = new SelectList(_context.BoardGames, "GameId", "GameName");
+            return View();
+        }
+
+        // POST: Meeting/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("MeetingId,MeetingName,DateOfMeeting,AddresOfMeeting,Description,AdditionalRequirements,PhotoLink,CreatorId,GameId")] Meeting meeting)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(meeting);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CreatorId"] = new SelectList(_context.Users, "Id", "Id", meeting.CreatorId);
+            ViewData["GameId"] = new SelectList(_context.BoardGames, "GameId", "GameName", meeting.GameId);
+            return View(meeting);
+        }
+
+        // GET: Meeting/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var meeting = await _context.Meetings.FindAsync(id);
+            if (meeting == null)
+            {
+                return NotFound();
+            }
+            ViewData["CreatorId"] = new SelectList(_context.Users, "Id", "Id", meeting.CreatorId);
+            ViewData["GameId"] = new SelectList(_context.BoardGames, "GameId", "GameName", meeting.GameId);
+            return View(meeting);
+        }
+
+        // POST: Meeting/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("MeetingId,MeetingName,DateOfMeeting,AddresOfMeeting,Description,AdditionalRequirements,PhotoLink,CreatorId,GameId")] Meeting meeting)
+        {
+            if (id != meeting.MeetingId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(meeting);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!MeetingExists(meeting.MeetingId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            ViewData["CreatorId"] = new SelectList(_context.Users, "Id", "Id", meeting.CreatorId);
+            ViewData["GameId"] = new SelectList(_context.BoardGames, "GameId", "GameName", meeting.GameId);
+            return View(meeting);
+        }
+
+        // GET: Meeting/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var meeting = await _context.Meetings
+                .Include(m => m.Creator)
+                .Include(m => m.Game)
+                .FirstOrDefaultAsync(m => m.MeetingId == id);
+            if (meeting == null)
+            {
+                return NotFound();
+            }
+
+            return View(meeting);
+        }
+
+        // POST: Meeting/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var meeting = await _context.Meetings.FindAsync(id);
+            _context.Meetings.Remove(meeting);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool MeetingExists(int id)
+        {
+            return _context.Meetings.Any(e => e.MeetingId == id);
+        }
+    }
+}
