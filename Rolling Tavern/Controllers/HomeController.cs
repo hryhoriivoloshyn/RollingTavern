a24827c2d4778f9bc85cff0b950821b3b7c0db31
Daniil Haiduk <danil.gayduk2002@gmail.com>
2021-11-12 17:12:31 +0200

    CreateMeetingPage initialization Design

--- Rolling Tavern/Controllers/HomeController.cs
+++ Rolling Tavern/Controllers/HomeController.cs
@@ -22,7 +22,11 @@ namespace Rolling_Tavern.Controllers
         {
             return View();
         }
-     
+        public IActionResult CreateMeetingPage()
+        {
+            return View();
+        }
+
         public IActionResult Privacy()
         {
             return View();
