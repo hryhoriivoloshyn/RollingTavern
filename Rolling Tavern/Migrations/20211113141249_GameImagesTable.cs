0208ef7e02a3729d6ceca84d081c0a192b72c9fe
Hryhorii-V <hryhorii.voloshyn@nure.ua>
2021-11-13 16:17:22 +0200

    Создана таблица GameImages

---
+++ Rolling Tavern/Migrations/20211113141249_GameImagesTable.cs
@@ -0,0 +1,41 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Rolling_Tavern.Migrations
+{
+    public partial class GameImagesTable : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "GameImages",
+                columns: table => new
+                {
+                    ImageId = table.Column<long>(type: "bigint", nullable: false)
+                        .Annotation("SqlServer:Identity", "1, 1"),
+                    ImagePath = table.Column<string>(type: "nvarchar(450)", maxLength: 450, nullable: false),
+                    GameId = table.Column<int>(type: "int", nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_GameImages", x => x.ImageId);
+                    table.ForeignKey(
+                        name: "FK_Games_Images",
+                        column: x => x.GameId,
+                        principalTable: "BoardGames",
+                        principalColumn: "GameId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_GameImages_GameId",
+                table: "GameImages",
+                column: "GameId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "GameImages");
+        }
+    }
+}
---
+++ Rolling Tavern/Models/GameImage.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Rolling_Tavern.Models
+{
+    public partial class GameImage
+    {
+        public long ImageId { get; set; }
+        public string ImagePath { get; set; }
+
+        public int? GameId { get; set; }
+
+        public virtual BoardGame Game { get; set; }
+    }
+}
