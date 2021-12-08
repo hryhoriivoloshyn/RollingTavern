0bba6b419ddd27dda8f38ff6e779268009d42083
daywish <volodymyr.korobko@nure.ua>
2021-12-08 15:40:34 +0200

    created branch for board games

---
+++ Rolling Tavern/Controllers/BoardGamesController.cs
@@ -0,0 +1,160 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using Rolling_Tavern.Data;
+using Rolling_Tavern.Models;
+
+namespace Rolling_Tavern.Controllers
+{
+    public class BoardGamesController : Controller
+    {
+        private readonly ApplicationDbContext _context;
+
+        public BoardGamesController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<List<BoardGame>> GetBoardGamesAsync()
+        {
+            List<BoardGame> tempData = await _context.BoardGames.Where(i=>i.GameId!=1).ToListAsync();
+            return tempData;
+        }
+
+        // GET: BoardGames
+        public async Task<IActionResult> Index()
+        {
+            var data = await GetBoardGamesAsync();
+            return View(data);
+        }
+
+        // GET: BoardGames/Details/5
+        public async Task<IActionResult> Details(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var boardGame = await _context.BoardGames
+                .FirstOrDefaultAsync(m => m.GameId == id);
+            if (boardGame == null)
+            {
+                return NotFound();
+            }
+
+            return View(boardGame);
+        }
+
+        // GET: BoardGames/Create
+        public IActionResult Create()
+        {
+            return View();
+        }
+
+        // POST: BoardGames/Create
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Create([Bind("GameId,GameName,Genre,Description,MinAmountOfPlayers,MaxAmountOfPlayers,MinGameTime,MaxGameTime,MinAgeOfPlayers")] BoardGame boardGame)
+        {
+            if (ModelState.IsValid)
+            {
+                _context.Add(boardGame);
+                await _context.SaveChangesAsync();
+                return RedirectToAction(nameof(Index));
+            }
+            return View(boardGame);
+        }
+
+        // GET: BoardGames/Edit/5
+        public async Task<IActionResult> Edit(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var boardGame = await _context.BoardGames.FindAsync(id);
+            if (boardGame == null)
+            {
+                return NotFound();
+            }
+            return View(boardGame);
+        }
+
+        // POST: BoardGames/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to.
+        // For more details, see http://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Edit(int id, [Bind("GameId,GameName,Genre,Description,MinAmountOfPlayers,MaxAmountOfPlayers,MinGameTime,MaxGameTime,MinAgeOfPlayers")] BoardGame boardGame)
+        {
+            if (id != boardGame.GameId)
+            {
+                return NotFound();
+            }
+
+            if (ModelState.IsValid)
+            {
+                try
+                {
+                    _context.Update(boardGame);
+                    await _context.SaveChangesAsync();
+                }
+                catch (DbUpdateConcurrencyException)
+                {
+                    if (!BoardGameExists(boardGame.GameId))
+                    {
+                        return NotFound();
+                    }
+                    else
+                    {
+                        throw;
+                    }
+                }
+                return RedirectToAction(nameof(Index));
+            }
+            return View(boardGame);
+        }
+
+        // GET: BoardGames/Delete/5
+        public async Task<IActionResult> Delete(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            var boardGame = await _context.BoardGames
+                .FirstOrDefaultAsync(m => m.GameId == id);
+            if (boardGame == null)
+            {
+                return NotFound();
+            }
+
+            return View(boardGame);
+        }
+
+        // POST: BoardGames/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> DeleteConfirmed(int id)
+        {
+            var boardGame = await _context.BoardGames.FindAsync(id);
+            _context.BoardGames.Remove(boardGame);
+            await _context.SaveChangesAsync();
+            return RedirectToAction(nameof(Index));
+        }
+
+        private bool BoardGameExists(int id)
+        {
+            return _context.BoardGames.Any(e => e.GameId == id);
+        }
+    }
+}
